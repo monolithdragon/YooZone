11f1c3726ad62060284ce6585f491eb6481d3886
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-09-21 17:26:00 +0200

    Create Hierarchy Icon utility

---
+++ YooZone/Hierarchy Utilities/HierarchyIconDisplay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace YooZone.HierarchyUtilities {
+    [InitializeOnLoad]
+    public static class HierarchyIconDisplay {
+        private static bool hasFocusWindow = false;
+        private static EditorWindow hierarchyEditorWindow;
+
+        static HierarchyIconDisplay() {
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
+            EditorApplication.update += OnEditorUpdate;
+        }
+
+        private static void OnHierarchyWindowItemOnGUI(int instanceID, Rect selectionRect) {
+            var instanceObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+            if (instanceObject == null)
+                return;
+
+            if (PrefabUtility.GetCorrespondingObjectFromOriginalSource(instanceObject) != null)
+                return;
+
+            var components = instanceObject.GetComponents<Component>();
+            if (components == null || components.Length == 0)
+                return;
+
+            var component = components.Length > 1 ? components[1] : components[0];
+            var type = component.GetType();
+            var content = EditorGUIUtility.ObjectContent(component, type);
+            content.text = null;
+            content.tooltip = type.Name;
+
+            if (content.image == null)
+                return;
+
+            var isSelected = Selection.instanceIDs.Contains(instanceID);
+            var isHovering = selectionRect.Contains(Event.current.mousePosition);
+
+            var color = UnityEditorBackgroundColor.GetColor(isSelected, isHovering, hasFocusWindow);
+            var backgroundRect = selectionRect;
+            backgroundRect.width = 18.5f;
+            EditorGUI.DrawRect(backgroundRect, color);
+
+            EditorGUI.LabelField(selectionRect, content);
+        }
+
+        private static void OnEditorUpdate() {
+            if (hierarchyEditorWindow == null)
+                hierarchyEditorWindow = EditorWindow.GetWindow(Type.GetType("UnityEditor.SceneHierarchyWindow,UnityEditor"));
+
+            hasFocusWindow = EditorWindow.focusedWindow != null && EditorWindow.focusedWindow == hierarchyEditorWindow;
+        }
+    }
+}
---
+++ YooZone/Hierarchy Utilities/UnityEditorBackgroundColor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace YooZone.HierarchyUtilities {
+    public static class UnityEditorBackgroundColor {
+        private readonly static Color defaultColor = new Color(0.7843f, 0.7843f, 0.7843f);
+        private readonly static Color defaultProColor = new Color(0.2196f, 0.2196f, 0.2196f);
+        private readonly static Color selectedColor = new Color(0.22745f, 0.447f, 0.6902f);
+        private readonly static Color selectedProColor = new Color(0.1725f, 0.3647f, 0.5294f);
+        private readonly static Color selectedUnFocusColor = new Color(0.68f, 0.68f, 0.68f);
+        private readonly static Color selectedUnFocusProColor = new Color(0.3f, 0.3f, 0.3f);
+        private readonly static Color hoveredColor = new Color(0.698f, 0.698f, 0.698f);
+        private readonly static Color hoveredProColor = new Color(0.2706f, 0.2706f, 0.2706f);
+
+        public static Color GetColor(bool isSelected, bool isHovered, bool isWindowFocused) {
+            if (isSelected) {
+                if (isWindowFocused) {
+                    return EditorGUIUtility.isProSkin ? selectedProColor : selectedColor;
+                } else {
+                    return EditorGUIUtility.isProSkin ? selectedUnFocusProColor : selectedUnFocusColor;
+                }
+            } else if (isHovered) {
+                return EditorGUIUtility.isProSkin ? hoveredProColor : hoveredColor;
+            }
+
+            return EditorGUIUtility.isProSkin ? defaultProColor : defaultColor;
+        }
+    }
+}
