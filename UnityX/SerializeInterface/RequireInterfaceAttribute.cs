16449a1f878eeb6865da928b4da34bef8e028a52
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2025-01-03 17:06:03 +0100

    Create serialize interface

---
+++ UnityX/SerializeInterface/RequireInterfaceAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityX.SerializeInterface {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class RequireInterfaceAttribute : PropertyAttribute {
+        public readonly Type InterfaceType;
+
+        public RequireInterfaceAttribute(Type interfaceType) {
+            Debug.Assert(interfaceType.IsInterface, $"{nameof(interfaceType)} needs to be an interface.");
+            InterfaceType = interfaceType;
+        }
+    }
+}
