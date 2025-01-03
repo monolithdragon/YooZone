fabfe01d62a7a05a7ae10475d968ae558ae85987
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2025-01-03 16:44:24 +0100

    Create Unity extensions

---
+++ UnityX/Extensions/AsyncOperationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityX {
+    public static class AsyncOperationExtensions {
+        /// <summary>
+        /// Extension method that converts an AsyncOperation into a Task.
+        /// </summary>
+        /// <param name="asyncOperation">The AsyncOperation to convert.</param>
+        /// <returns>A Task that represents the completion of the AsyncOperation.</returns>
+        public static Task AsTask(this AsyncOperation asyncOperation) {
+            var taskCompleteSource = new TaskCompletionSource<bool>();
+            asyncOperation.completed += _ => taskCompleteSource.SetResult(true);
+            return taskCompleteSource.Task;
+        }
+    }
+}
---
+++ UnityX/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnityX {
+    public static class DateTimeExtensions {
+        /// <summary>
+        /// Returns a new <see cref="DateTime"/> object with the specified year, month, and day.
+        /// If any of the parameters are null, the corresponding value from the original <see cref="DateTime"/> is used.
+        /// </summary>
+        /// <param name="dateTime">The original <see cref="DateTime"/> object.</param>
+        /// <param name="year">The new year value. If null, the original year is used.</param>
+        /// <param name="month">The new month value. If null, the original month is used.</param>
+        /// <param name="day">The new day value. If null, the original day is used.</param>
+        /// <returns>A new <see cref="DateTime"/> object with the specified year, month, and day.</returns>
+        public static DateTime WithDate(this DateTime dateTime, int? year = null, int? month = null, int? day = null) {
+            int newYear = year ?? dateTime.Year;
+            int newMonth = month ?? dateTime.Month;
+            int newDay = day ?? dateTime.Day;
+
+            // Ensure the new date is valid by clamping day if necessary
+            int daysInMonth = DateTime.DaysInMonth(newYear, newMonth);
+            newDay = Math.Min(newDay, daysInMonth);
+
+            return new DateTime(newYear, newMonth, newDay, dateTime.Hour, dateTime.Minute, dateTime.Second, dateTime.Millisecond);
+        }
+    }
+}
